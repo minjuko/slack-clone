b3f5eb8b51042c0937497d4671630dfcfa6568f8
minjuko <rhalswn0526@gmail.com>
2024-01-29 13:06:37 +0900

    feat: firebase 회원가입과 연동

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import {
   Alert,
   Avatar,
@@ -9,9 +9,78 @@ import {
   Typography,
 } from "@mui/material";
 import TagIcon from "@mui/icons-material/Tag";
+import { LoadingButton } from "@mui/lab";
 import { Link } from "react-router-dom";
+import "../firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import md5 from "md5";
+import { getDatabase, ref, set } from "firebase/database";
+
+const IsPasswordValid = (password, confirmPassword) => {
+  if (password.length < 6 || confirmPassword.length < 6) {
+    return false;
+  } else if (password !== confirmPassword) {
+    return false;
+  } else {
+    return true;
+  }
+};
 function Join() {
-     return (
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const name = data.get("name");
+    const email = data.get("email");
+    const password = data.get("password");
+    const confirmPassword = data.get("confirmPassword");
+
+    if (!name || !email || !password || !confirmPassword) {
+      setError("모든 항목을 입력해주세요.");
+      return;
+    }
+
+    if (!IsPasswordValid(password, confirmPassword)) {
+      setError("비밀번호를 확인해주세요.");
+      return;
+    }
+
+    postUserData(name, email, password);
+  };
+
+  const postUserData = async (name, email, password) => {
+    setLoading(true);
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        getAuth(),
+        email,
+        password
+      );
+      await updateProfile(user, {
+        displayName: name,
+        photoURL: `https://www.gravatar.com/avatar/${md5(email)}?d=retro`,
+      });
+      await set(ref(getDatabase(), "users/" + user.uid), {
+        name: user.displayName,
+        avatar: user.photoURL,
+      });
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  useEffect(() => {
+    if (!error) return;
+    setTimeout(() => {
+      setError("");
+    }, 3000);
+  }, [error]);
+  return (
     <Container component="main" maxWidth="xs">
       <Box
         sx={{
@@ -77,12 +146,11 @@ function Join() {
             type="submit"
             fullWidth
             variant="contained"
-            color="secondary"
-            loading={loading}
             sx={{ mt: 3, mb: 2 }}
           >
             회원가입
           </LoadingButton>
+
           <Grid container justifyContent="flex-end">
             <Grid item>
               <Link
@@ -99,4 +167,4 @@ function Join() {
   );
 }
 
-export default Join
+export default Join;
