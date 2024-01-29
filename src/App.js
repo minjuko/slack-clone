cadd9cbaa8af838aa965a71b600eb18eddcd1dc8
minjuko <rhalswn0526@gmail.com>
2024-01-29 13:06:17 +0900

    feat: Login 페이지 경로

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Join from "./pages/Join";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/join" element={<Join/>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/join" element={<Join />} />
     </Routes>
   );
 }
